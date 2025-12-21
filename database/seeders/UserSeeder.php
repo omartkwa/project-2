5a0b8682dd5a2a1916219491863062a96921d59d
Omar Tkwa <omartkwa123@gmail.com>
2025-12-21 08:23:22 +0300

    seeder 2

--- database/seeders/UserSeeder.php
+++ database/seeders/UserSeeder.php
@@ -14,26 +14,17 @@ class UserSeeder extends Seeder
      */
     public function run(): void
     {
-         DB::table('users')->insert([
-            'first_name' => 'Admin',
-            'last_name' => 'User',
-            'role' => 'admin',
-            'birthdate' => null,
-            'profile_photo' => null,
-            'id_photo' => null,
-            'is_approved' => true,
-            'is_active' => true,
-            'mobile' => '0123456789',
-            'address' => null,
-            'card_type' => null,
-            'card_number' => null,
-            'security_code' => null,
-            'expiry_date' => null,
-            'account' => 0,
-            'email_verified_at' => now(),
-            'password' => Hash::make('12345678'), // غير كلمة المرور حسب الحاجة
-            'created_at' => now(),
-            'updated_at' => now(),
-        ]);
+      DB::table('users')->insert([
+    'first_name' => 'Admin',
+    'last_name' => 'User',
+    'role' => 'admin',
+    'mobile' => '0123456789',
+    'account' => 0,
+    'password' => Hash::make('12345678'),
+    'is_approved' => true,
+    'is_active' => true,
+    'created_at' => now(),
+    'updated_at' => now(),
+]);
     }
 }
